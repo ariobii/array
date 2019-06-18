4cdd9ff812996d475df85a96838f1bd100a6a369
ariobii <auliabimo5@gmail.com>
2019-06-18 14:54:20 +0700

    arrrraaaaaayyy

---
+++ Array/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arrayNama = { "aa", "bb", "cc", "dd" };
+            Console.WriteLine(arrayNama[2] + "\n");
+
+            for(int i = 0; i < arrayNama.Length; i++)
+            {
+                Console.WriteLine(arrayNama[i]);
+            }
+            Console.WriteLine();
+
+            foreach(string nama in arrayNama)
+            {
+                Console.WriteLine(nama);
+            }
+            Console.WriteLine();
+
+            int[] arrayBilangan = new int[3];
+
+            arrayBilangan[0] = 1;
+            arrayBilangan[1] = 2;
+            arrayBilangan[2] = 3;
+
+            foreach(int bil in arrayBilangan)
+            {
+                Console.WriteLine(bil.ToString());
+            }
+        }
+    }
+}
---
+++ Array2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Array2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mahasiswa mhs1 = new Mahasiswa();
+            mhs1.Nama = "Aulia";
+            mhs1.Nilai = 90;
+            mhs1.Nim = "2279";
+
+            Mahasiswa mhs2 = new Mahasiswa();
+            mhs2.Nama = "Bimo ";
+            mhs2.Nilai = 95;
+            mhs2.Nim = "2277";
+
+            Mahasiswa mhs3 = new Mahasiswa();
+            mhs3.Nama = "Ario ";
+            mhs3.Nilai = 85;
+            mhs3.Nim = "2278";
+
+            Mahasiswa[] arrayMhs = new Mahasiswa[3];
+            arrayMhs[0] = mhs1;
+            arrayMhs[1] = mhs2;
+            arrayMhs[2] = mhs3;
+
+            Console.WriteLine("No. NIM    Nama    Nilai");
+            Console.WriteLine("--------------------------");
+            
+            //Pakai for
+            /*
+            for(int i=0; i < arrayMhs.Length; i++)
+            {
+                Console.WriteLine("{0}   {1}    {2}     {3}", i + 1, arrayMhs[i].Nim, arrayMhs[i].Nama, arrayMhs[i].Nilai);
+            }
+            */
+
+            //Pakai foreach
+            int j=1;
+            foreach(Mahasiswa mhs in arrayMhs)
+            {
+                Console.WriteLine("{0}   {1}   {2}   {3}", j, mhs.Nim, mhs.Nama, mhs.Nilai);
+                j++;
+            }
+        }
+    }
+}
